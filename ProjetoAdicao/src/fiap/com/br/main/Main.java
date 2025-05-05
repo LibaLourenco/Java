1efeddbd3a1c5d25b3aba8a307296e17c8fb896b
Aline Lourenço <libalourenco@icloud.com>
2025-05-05 10:21:47 -0300

    Add files via upload

---
+++ ProjetoAdicao/src/fiap/com/br/main/Main.java
@@ -0,0 +1,13 @@
+package fiap.com.br.main;
+
+import fiap.com.br.bean.Adicao;
+
+public class Main {
+    public static void main(String[] args) {
+    Adicao adicao1 = new Adicao();
+
+
+
+
+    }
+}
